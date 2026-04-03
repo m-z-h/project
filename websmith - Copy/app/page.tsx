8e98cd7ef5bbd3d4fdac0411cea9f0525d5d71cf
Md Zahid Hussain <174180646+m-z-h@users.noreply.github.com>
2026-04-03 19:14:36 +0530

    Delete websmith - Copy directory

--- websmith - Copy/app/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function HomePage() {
-return ( <div> <h1>Home</h1> <p>Go to /login</p> </div>
-);
-}
--- websmith - Copy/backend/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getStats } = require('../controllers/statsController');
-
-router.get('/', protect, getStats);
-
-module.exports = router;
